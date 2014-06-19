841dd0e84b5b8f27210c72439ae416b5c77ebc17
Seth Geyer <seth.geyer@gmail.com>
2014-06-19 14:00:13 -0600

    original commit

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -1,3 +1,10 @@
 require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
+
+address = "444 Borg Lane, San Francisco, CA, 94104"
+
+bjs_address = "#{BJORN_BORG["address"]["street"]}, #{ BJORN_BORG["address"]["city"]}, #{BJORN_BORG["address"]["state"]}, #{BJORN_BORG["address"]["zip_code"]}"
+
+puts
+puts bjs_address if address == bjs_address
