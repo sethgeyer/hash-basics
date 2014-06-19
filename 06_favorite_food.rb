2fb27c4ca9ad7b3f519a3ef583e27e6c1b8007fc
Seth Geyer <seth.geyer@gmail.com>
2014-06-19 14:13:12 -0600

    original commit

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -1,3 +1,8 @@
 require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
+
+faves = BJORN_BORG["favorite_foods"]
+faves[-1] = "and #{faves[-1]}"
+
+puts "Bjorn's favorite foods are #{faves.join(", ")}."
